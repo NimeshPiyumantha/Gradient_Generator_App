e016bc4e24275557d44d11c9b02bddad2513cd34
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-27 16:39:46 +0530

    Update Content

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Gradient_Generator_App/",
   build: {
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
